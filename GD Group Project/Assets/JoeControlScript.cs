f1188d57d913613f6c7335b701333ce5f53b7fc7
STUDENT Stefan Lajtar <Stefan.Lajtar@students.ittralee.ie>
2022-10-04 14:39:41 +0100

    Signed-off-by: STUDENT Stefan Lajtar <Stefan.Lajtar@students.ittralee.ie>

--- GD Group Project/Assets/JoeControlScript.cs
+++ GD Group Project/Assets/JoeControlScript.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+#
 public class JoeControlScript : MonoBehaviour
 {
     enum CharacterStates {Grounded, JumpUp, Falling }
