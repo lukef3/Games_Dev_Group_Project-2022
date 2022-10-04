8403ca35e3dfa7a352f9e7bf2d5188e25c98e8a6
STUDENT Luke Foley <Luke.Foley@students.ittralee.ie>
2022-10-04 15:23:06 +0100

    testing Signed-off-by: STUDENT Luke Foley <Luke.Foley@students.ittralee.ie>

--- GD Group Project/Assets/CubeControlScript.cs
+++ GD Group Project/Assets/CubeControlScript.cs
@@ -5,28 +5,35 @@ using TMPro;
 
 public class CubeControlScript : MonoBehaviour
 {
-    public TMP_Text healthText;
-    int health = 100;
+    public TMP_Text FloatingText;
+    public ParticleSystem Explosion;
+
+    TimerScript my_timer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        my_timer = gameObject.AddComponent<TimerScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space)) { 
+            my_timer.setCooldown(10);
+        }
+
+        UpdateTextBoxes();
+        print(my_timer.RemainingTime);
+
+        if(my_timer.RemainingTime == 0)
+        {
+            Explosion.Play();
+        }
     }
 
     void UpdateTextBoxes()
     {
-        healthText.text = health.ToString();
-    }
-
-    private void OnMouseDown()
-    {
-        health -= 1;
-        UpdateTextBoxes();
+        FloatingText.text = my_timer.RemainingTime.ToString("0");
     }
 }
